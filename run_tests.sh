1f471dc16ad5fbd805329278d2b2a8e4dfacf9f4
Kevin Cheng <kevcheng@google.com>
2018-05-30 00:04:32 -0700

    Update run_tests.sh and remove from PREUPLOAD.cfg.
    run_tests.sh updated to:
    - Make coverage run by default
    - Run coverage through python since coverage may not be in PATH
    - Create coverage report in /tmp and print out a link to coverage report
    - Add check for run_tests.sh to make sure req python libs are installed.
    
    I removed run_tests.sh from PREUPLOAD.cfg because those checks
    shouldn't:
    - require anything outside the src tree
    - require a build env
    - write to the f/s
    The unittests should be run as part of Treehugger and so a subsequent cl
    will be coming to fix that.
    
    Bug: 110430047
    Test: ./run_tests.sh
    Change-Id: I113bd2820eeb2fc9e502d6613113bd16947c0c5e

--- run_tests.sh
+++ run_tests.sh
@@ -5,24 +5,14 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-if [ -z "$ANDROID_BUILD_TOP" ]; then
-    echo "Missing ANDROID_BUILD_TOP env variable. Run 'lunch' first."
-    exit 1
-fi
-
-function helper() {
-    echo "usage: $0 [options]"
-    echo "  options:"
-    echo "    coverage, test all unit tests with coverage report"
-}
-
 function print_summary() {
     local test_results=$1
-    local coverage_run=$2
-    if [[ $coverage_run == "coverage" ]]; then
-        PYTHONPATH=$(get_python_path) coverage report -m
-        PYTHONPATH=$(get_python_path) coverage html
-    fi
+    local tmp_dir=$(mktemp -d)
+    local rc_file=${ACLOUD_DIR}/.coveragerc
+    PYTHONPATH=$(get_python_path) python -m coverage report -m
+    PYTHONPATH=$(get_python_path) python -m coverage html -d $tmp_dir --rcfile=$rc_file
+    echo "coverage report available at file://${tmp_dir}/index.html"
+
     if [[ $test_results -eq 0 ]]; then
         echo -e "${GREEN}All unittests pass${NC}!"
     else
@@ -31,38 +21,43 @@ function print_summary() {
 }
 
 function run_unittests() {
-    local coverage_run=$1
-    local run_cmd="python"
     local rc=0
-    if [[ $coverage_run == "coverage" ]]; then
-        # clear previously collected coverage data.
-        PYTHONPATH=$(get_python_path) coverage erase
-        run_cmd="coverage run --append"
-    fi
+    local run_cmd="python -m coverage run --append"
+
+    # clear previously collected coverage data.
+    PYTHONPATH=$(get_python_path) python -m coverage erase
 
     # Runs all unit tests under tools/acloud.
     for t in $(find $ACLOUD_DIR -type f -name "*_test.py");
     do
-        if ! PYTHONPATH=$(get_python_path) $run_cmd $t; then
+        if ! PYTHONPATH=$(get_python_path):$PYTHONPATH $run_cmd $t; then
             rc=1
             echo -e "${RED}$t failed${NC}"
         fi
     done
 
-    print_summary $rc $coverage_run
+    print_summary $rc
     exit $rc
 }
 
+function check_env() {
+    if [ -z "$ANDROID_BUILD_TOP" ]; then
+        echo "Missing ANDROID_BUILD_TOP env variable. Run 'lunch' first."
+        exit 1
+    fi
 
-case "$1" in
-    'help')
-        helper
-        ;;
-    'coverage')
-        run_unittests "coverage"
-        ;;
-    *)
-        run_unittests
-        ;;
-esac
+    local missing_py_packages=false
+    for py_lib in {absl-py,coverage,mock};
+    do
+        if ! pip list --format=legacy | grep $py_lib &> /dev/null; then
+            echo "Missing required python package: $py_lib (pip install $py_lib)"
+            missing_py_packages=true
+        fi
+    done
+    if $missing_py_packages; then
+        exit 1
+    fi
+}
 
+check_env
+run_unittests
