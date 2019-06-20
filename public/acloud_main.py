37b1ee3ba2be83752147474cb211bd026ce2c138
Sam Chiu <samchiu@google.com>
2019-06-20 10:49:56 +0800

    Add an extra arg --no-metrics to skip metrics
    Bug: 133215912
    Test: atest acloud_test
    acloud-dev list/create/reconnect/delete --no-metrics
    
    Change-Id: I3fee81abb93e6b5a2e9e709c2101f006b28e859a

--- public/acloud_main.py
+++ public/acloud_main.py
@@ -484,7 +484,7 @@ if __name__ == "__main__":
     EXIT_CODE = None
     EXCEPTION_STACKTRACE = None
     EXCEPTION_LOG = None
-    metrics.LogUsage(sys.argv[1:])
+    LOG_METRICS = metrics.LogUsage(sys.argv[1:])
     try:
         EXIT_CODE = main(sys.argv[1:])
     except Exception as e:
@@ -494,6 +494,7 @@ if __name__ == "__main__":
         raise
     finally:
         # Log Exit event here to calculate the consuming time.
-        metrics.LogExitEvent(EXIT_CODE,
-                             stacktrace=EXCEPTION_STACKTRACE,
-                             logs=EXCEPTION_LOG)
+        if LOG_METRICS:
+            metrics.LogExitEvent(EXIT_CODE,
+                                 stacktrace=EXCEPTION_STACKTRACE,
+                                 logs=EXCEPTION_LOG)
