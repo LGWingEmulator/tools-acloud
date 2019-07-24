f217fff7173ed942bc543b1a14fe758a5fd7fae4
herbertxue <herbertxue@google.com>
2019-07-24 16:01:10 +0800

    Refactor RemoteInstanceDeviceFactory out of local_image_remote_instance.py
    We're extracting the factory so that it can be utilized by moving
    RemoteInstanceDeviceFactory to remote_instance_cf_device_factory.py
    
    Bug:134919522
    Test: acloud-dev create --local-image
    Change-Id: Ife131c6570566dbe69c8ce6296b366078b0c5104

--- create/local_image_remote_instance_test.py
+++ create/local_image_remote_instance_test.py
@@ -18,22 +18,14 @@ r"""LocalImageRemoteInstance class.
 Create class that is responsible for creating a remote instance AVD with a
 local image.
 """
-import uuid
 
-import glob
 import os
-import subprocess
-import time
 import unittest
 
 import mock
 
 from acloud import errors
-from acloud.create import avd_spec
 from acloud.create import local_image_remote_instance
-from acloud.internal import constants
-from acloud.internal.lib import auth
-from acloud.internal.lib import cvd_compute_client
 from acloud.internal.lib import driver_test_lib
 from acloud.internal.lib import utils
 
@@ -74,76 +66,6 @@ class LocalImageRemoteInstanceTest(driver_test_lib.BaseDriverTest):
             self.assertEqual(
                 self.local_image_remote_instance.VerifyHostPackageArtifactsExist(),
                 "/fake_dir2/cvd-host_package.tar.gz")
-
-
-class RemoteInstanceDeviceFactoryTest(driver_test_lib.BaseDriverTest):
-    """Test RemoteInstanceDeviceFactory method."""
-
-    def setUp(self):
-        """Set up the test."""
-        super(RemoteInstanceDeviceFactoryTest, self).setUp()
-        self.Patch(auth, "CreateCredentials", return_value=mock.MagicMock())
-        self.Patch(cvd_compute_client.CvdComputeClient, "InitResourceHandle")
-
-    # pylint: disable=protected-access
-    def testSSHExecuteWithRetry(self):
-        """test SSHExecuteWithRetry method."""
-        self.Patch(time, "sleep")
-        factory = local_image_remote_instance.RemoteInstanceDeviceFactory
-        self.Patch(subprocess, "check_call",
-                   side_effect=subprocess.CalledProcessError(
-                       None, "ssh command fail."))
-        self.assertRaises(subprocess.CalledProcessError,
-                          factory._ShellCmdWithRetry,
-                          "fake cmd")
-        self.assertEqual(subprocess.check_call.call_count, #pylint: disable=no-member
-                         local_image_remote_instance._SSH_CMD_MAX_RETRY + 1)
-        self.Patch(subprocess, "check_call", return_value=True)
-        self.assertEqual(factory._ShellCmdWithRetry("fake cmd"), True)
-
-    # pylint: disable=protected-access
-    @mock.patch.dict(os.environ, {constants.ENV_BUILD_TARGET:'fake-target'})
-    def testCreateGceInstanceName(self):
-        """test create gce instance."""
-        self.Patch(utils, "GetBuildEnvironmentVariable",
-                   return_value="test_environ")
-        self.Patch(glob, "glob", return_vale=["fake.img"])
-        # Mock uuid
-        args = mock.MagicMock()
-        args.config_file = ""
-        args.avd_type = constants.TYPE_CF
-        args.flavor = "phone"
-        args.local_image = None
-        args.adb_port = None
-        fake_avd_spec = avd_spec.AVDSpec(args)
-
-        fake_uuid = mock.MagicMock(hex="1234")
-        self.Patch(uuid, "uuid4", return_value=fake_uuid)
-        self.Patch(cvd_compute_client.CvdComputeClient, "CreateInstance")
-        fake_host_package_name = "/fake/host_package.tar.gz"
-        fake_image_name = "/fake/aosp_cf_x86_phone-img-eng.username.zip"
-
-        factory = local_image_remote_instance.RemoteInstanceDeviceFactory(
-            fake_avd_spec,
-            fake_image_name,
-            fake_host_package_name)
-        self.assertEqual(factory._CreateGceInstance(), "ins-1234-userbuild-aosp-cf-x86-phone")
-
-        # Can't get target name from zip file name.
-        fake_image_name = "/fake/aosp_cf_x86_phone.username.zip"
-        factory = local_image_remote_instance.RemoteInstanceDeviceFactory(
-            fake_avd_spec,
-            fake_image_name,
-            fake_host_package_name)
-        self.assertEqual(factory._CreateGceInstance(), "ins-1234-userbuild-fake-target")
-
-        # No image zip path, it uses local build images.
-        fake_image_name = ""
-        factory = local_image_remote_instance.RemoteInstanceDeviceFactory(
-            fake_avd_spec,
-            fake_image_name,
-            fake_host_package_name)
-        self.assertEqual(factory._CreateGceInstance(), "ins-1234-userbuild-fake-target")
 
 
 if __name__ == "__main__":
