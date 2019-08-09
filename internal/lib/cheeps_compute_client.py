f3fce77f1e7ca35ee85f096ee3da7201d384df04
cylan <cylan@google.com>
2019-08-09 18:04:52 +0800

    Make GCE users consistently use vsoc-01.
    - Refactor sshkey metadata logic as a base method in android_compute_client.
    
    Bug:137325264
    Bug:134919522
    Test: acloud-dev create --local-image
          acloud-dev create
          acloud-dev create --multi-stage-launch
    
    Change-Id: I87fbb9684365b702d9bb6150106a34c342f95d43

--- internal/lib/cheeps_compute_client.py
+++ internal/lib/cheeps_compute_client.py
@@ -95,17 +95,6 @@ class CheepsComputeClient(android_compute_client.AndroidComputeClient):
                 metadata["user"] = avd_spec.username
                 metadata["password"] = avd_spec.password
 
-        # Add per-instance ssh key
-        if self._ssh_public_key_path:
-            rsa = self._LoadSshPublicKey(self._ssh_public_key_path)
-            logger.info("ssh_public_key_path is specified in config: %s, "
-                        "will add the key to the instance.",
-                        self._ssh_public_key_path)
-            metadata["sshKeys"] = "%s:%s" % (getpass.getuser(), rsa)
-        else:
-            logger.warning("ssh_public_key_path is not specified in config, "
-                           "only project-wide key will be effective.")
-
         # Add labels for giving the instances ability to be filter for
         # acloud list/delete cmds.
         labels = {constants.LABEL_CREATE_BY: getpass.getuser()}
